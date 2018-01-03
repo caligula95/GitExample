fb8f006daefe3e7184b6e212e67ae7a2411b75d2
Denis <burdden@gmail.com>
2018-01-03 20:47:22 +0200

    commit changes
    and some description for other members

--- src/GitExampleClass.java
+++ src/GitExampleClass.java
@@ -3,7 +3,8 @@ public class GitExampleClass {
 
 	public static void main(String[] args) {
 		System.out.println("some stuff out here");
-		
+		//assume that we made some changes here
+		System.out.println("and here");
 
 	}
 
